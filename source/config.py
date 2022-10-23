73f169b032d50c8714d0ead2b9c54c2655e5f04c
yxncc <90302132+yxncc@users.noreply.github.com>
2022-10-23 19:06:15 +0800

    initial commit

--- source/config.py
+++ source/config.py
@@ -13,7 +13,7 @@ class ProductionConfig(Config):
 class DevelopmentConfig(Config):
     SQLALCHEMY_POOL_TIMEOUT = 360
     DB_SERVER = 'localhost'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/postgres'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:W4L3R6Af@localhost:5433/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'your secret key'
     DEBUG = False
