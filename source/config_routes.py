6e6d0e11dc66e11b4c3fdfdaea3fbe22d0189606
sheftel <sheftel.ks@gmail.com>
2022-11-11 23:42:37 +0700

    Update routes

--- source/config_routes.py
+++ source/config_routes.py
@@ -1,11 +1,15 @@
 from flask_restful import Api
-from .resources import ProviderDetailResource, ProviderListResource, PositionDetailResource, PositionListResource
+from .resources import ProviderDetailResource, ProviderListResource, ProductDetailResource, ProductListResource,\
+    PositionDetailResource, PositionListResource
 
 
 def init_routes(app):
     api = Api(app)
     api.add_resource(ProviderListResource, '/providers/')
     api.add_resource(ProviderDetailResource, '/providers/<int:provider_id>')
+
+    api.add_resource(ProductListResource, '/products/')
+    api.add_resource(ProductDetailResource, '/products/<int:product_id>')
 
     api.add_resource(PositionListResource, '/positions/')
     api.add_resource(PositionDetailResource, '/positions/<int:position_id>')
--- source/resources/__init__.py
+++ source/resources/__init__.py
@@ -1,2 +1,3 @@
 from .provider_resource import ProviderDetailResource, ProviderListResource
 from .position_resource import PositionDetailResource, PositionListResource
+from .product_resource import ProductDetailResource, ProductListResource
