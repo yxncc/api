c0a60e7a9971c4569c2bac5d4ee54f511bc11276
sheftel <sheftel.ks@gmail.com>
2022-11-11 23:42:01 +0700

    Refactor product schema

--- source/schemas/product.py
+++ source/schemas/product.py
@@ -1,7 +1,19 @@
-from marshmallow import Schema, fields as mf
+from marshmallow import Schema, fields as mf, post_load
+
+from ..models import Product
 
 
 class ProductSchema(Schema):
-    id = mf.Integer(required=True)
+    id = mf.Integer()
+    name = mf.String(required=True)
     provider_id = mf.Integer(required=True)
 
+    class Meta:
+        ordered = True
+
+    @post_load
+    def make_position(self, data, **kwargs):
+        return Product(
+            name=data['name'],
+            provider_id=data['provider_id']
+        )
