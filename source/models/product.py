f2538a927bbcc73c3166cfaffa38f685ee8259ae
sheftel <sheftel.ks@gmail.com>
2022-11-11 23:28:12 +0700

    Update product model
    Update product model. Migrate changes to product model

--- source/models/product.py
+++ source/models/product.py
@@ -7,6 +7,7 @@ from ..extensions import db
 class Product(db.Model):
     __tablename__ = 'products'
     id = db.Column(Integer, primary_key=True)
+    name = db.Column(String, nullable=False)
     provider_id = db.Column(
         Integer,
         db.ForeignKey('providers.id'),
