c657ed4af6ec419b3138a38e2b187deea7a9fcf4
sheftel <sheftel.ks@gmail.com>
2022-11-12 14:41:11 +0700

    Refactor user schema

--- source/schemas/user.py
+++ source/schemas/user.py
@@ -1,9 +1,28 @@
-from marshmallow import Schema, fields as mf
+from marshmallow import Schema, fields as mf, post_load
+
+from . import OrderSchema
+from ..models import User
 
 
 class UserSchema(Schema):
     id = mf.Integer()
-    last_name = mf.String()
-    first_name = mf.String()
-    father_name = mf.String()
-    email = mf.String()
+    first_name = mf.String(required=True)
+    last_name = mf.String(required=True)
+    father_name = mf.String(required=True)
+    email = mf.String(required=True)
+
+    orders = mf.List(
+        mf.Nested(OrderSchema(), dump_only=True)
+    )
+
+    class Meta:
+        ordered = True
+
+    @post_load
+    def make_provider(self, data, **kwargs):
+        return User(
+            first_name=data['first_name'],
+            last_name=data['last_name'],
+            father_name=data['father_name'],
+            email=data['email'],
+        )
