e7c01baae4bcbd1e94a4751cbfb98587b3cf436a
sheftel <sheftel.ks@gmail.com>
2022-11-12 19:30:25 +0700

    Refactor order schema

--- source/schemas/order.py
+++ source/schemas/order.py
@@ -1,6 +1,8 @@
-from marshmallow import Schema, fields as mf
+from marshmallow import Schema, fields as mf, post_load, pre_load
 
 from . import ProductSchema
+from ..models import Order, Product
+from ..extensions import db
 
 
 class ReducedOrderSchema(Schema):
@@ -11,9 +13,20 @@ class ReducedOrderSchema(Schema):
 
 class OrderSchema(Schema):
     id = mf.Integer()
-    worker_id = mf.Integer()
-    user_id = mf.Integer()
-    content = mf.String()
+    worker_id = mf.Integer(required=True)
+    user_id = mf.Integer(required=True)
+    is_active = mf.Boolean(load_default=True)
+
+    product_ids = mf.List(mf.Integer(), required=True, load_only=True)
     products = mf.List(
-        mf.Nested(ProductSchema())
+        mf.Nested(ProductSchema(), dump_only=True)
     )
+
+    class Meta:
+        ordered = True
+
+    @post_load
+    def make_order(self, data, **kwargs):
+        data['products'] = [Product.query.get(product_id) for product_id in data['product_ids']]
+        return data
+
